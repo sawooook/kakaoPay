728270c57a36f407211cdac46beb237af1532c9c
saouk <lna1003@naver.com>
2021-04-12 21:22:20 +0900

    first init

---
+++ src/main/java/com/example/kakaoPay/KakaoPayApplication.java
@@ -0,0 +1,13 @@
+package com.example.kakaoPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoPayApplication.class, args);
+	}
+
+}
